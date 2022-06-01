b782cefdeb90eaf24ced4b037d82f2d606f8ad3f
zhansul19 <zhansultanjan@gmail.com>
2022-06-01 18:25:21 +0600

    implemented transfer transaction

--- db/store.go
+++ db/store.go
@@ -33,3 +33,52 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	}
 	return tx.Commit()
 }
+func (store *Store) transferTX(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfers, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	//TODO: update balances without deadlocks
+
+	return result, err
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_accounut_id"`
+	ToAccountID   int64 `json:"to_accounut_id"`
+	Amount        int64 `json:"amount"`
+}
+type TransferTxResult struct {
+	Transfers   Transfers `json:"transfers"`
+	FromAccoont Accounts  `json:"from_account"`
+	ToAccount   Accounts  `json:"to_account"`
+	FromEntry   Entries   `json:"from_entry"`
+	ToEntry     Entries   `json:"to_entry"`
+}
